7930d584523920f454a225d25259589d30c083db
parthkanani93 <parthkanani8490@gmail.com>
2021-03-10 22:56:35 +0530

    convert chatscreen to flatlist

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,7 +1,42 @@
 import React from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { CommonStyles } from "../styles/CommonStyles";
+
+const data = [
+  {
+    isLeft: true,
+    text: "Hello guys",
+  },
+  {
+    isLeft: false,
+    text: "I am new here",
+  },
+  {
+    isLeft: true,
+    text: "Ok Lol i am also",
+  },
+  {
+    isLeft: true,
+    text: "Yo yoo yo",
+  },
+  {
+    isLeft: true,
+    text: "Hello guys",
+  },
+  {
+    isLeft: false,
+    text: "I am new here",
+  },
+  {
+    isLeft: false,
+    text: "Ok Lol i am also",
+  },
+  {
+    isLeft: true,
+    text: "Yo yoo yo",
+  },
+];
 
 const ChatScreen = () => {
   return (
@@ -9,94 +44,48 @@ const ChatScreen = () => {
       <View style={styles.room}>
         <Text style={styles.roomtext}>Room name</Text>
       </View>
-      <ScrollView style={{ flex: 1 }}>
-        <View style={styles.recived}>
-          <Text
-            style={[
-              styles.text,
-              { fontSize: 15, fontFamily: "Poppins-SemiBold" },
-            ]}
-          >
-            Name
-          </Text>
-          <Text style={styles.text}>
-            Hello school mates welcome all to new chat room!! here we go
-          </Text>
-        </View>
-        <View style={styles.sender}>
+      <ScrollView style={styles.main_innerscroll}>
+        <FlatList
+          data={data}
+          renderItem={(item) => (
+            <View style={item.item.isLeft ? styles.recived : styles.sender}>
+              {item.item.isLeft && (
+                <Text
+                  style={[
+                    styles.text,
+                    { fontSize: 15, fontFamily: "Poppins-SemiBold" },
+                  ]}
+                >
+                  Name
+                </Text>
+              )}
+              <Text
+                style={
+                  item.item.isLeft
+                    ? styles.text
+                    : [styles.text, { color: "#fff" }]
+                }
+              >
+                {item.item.text}
+              </Text>
+            </View>
+          )}
+        />
+        {/* <View style={styles.sender}>
           <Text style={[styles.text, { color: "#fff" }]}>
             Hello guys here i am back with onther video
           </Text>
-        </View>
-        <View style={styles.recived}>
-          <Text
-            style={[
-              styles.text,
-              { fontSize: 15, fontFamily: "Poppins-SemiBold" },
-            ]}
-          >
-            Name
-          </Text>
-          <Text style={styles.text}>
-            Hello school mates welcome all to new chat room!! here we go
-          </Text>
-        </View>
-        <View style={styles.sender}>
-          <Text style={[styles.text, { color: "#fff" }]}>
-            Hello guys here i am back with onther video
-          </Text>
-        </View>
-        <View style={styles.recived}>
-          <Text
-            style={[
-              styles.text,
-              { fontSize: 15, fontFamily: "Poppins-SemiBold" },
-            ]}
-          >
-            Name
-          </Text>
-          <Text style={styles.text}>
-            Hello school mates welcome all to new chat room!! here we go
-          </Text>
-        </View>
-        <View style={styles.sender}>
-          <Text style={[styles.text, { color: "#fff" }]}>
-            Hello guys here i am back with onther video
-          </Text>
-        </View>
+        </View> */}
       </ScrollView>
 
-      <View style={styles.chatinput}>
+      <View style={styles.chatinput_container}>
         <TextInput
           placeholder="Type here..."
           placeholderTextColor="#292929"
-          style={
-            (CommonStyles.textInput,
-            {
-              backgroundColor: "#fff",
-              paddingVertical: "2%",
-              borderRadius: 7,
-              paddingHorizontal: "4%",
-              color: "black",
-              width: "85%",
-            })
-          }
+          style={[CommonStyles.textInput, styles.chatinput]}
           underlineColorAndroid="transparent"
-          // value={send}
-          // onChangeText={(uname) => setUsername(uname)}
         />
-        <TouchableOpacity
-          style={{
-            height: 44,
-            width: 44,
-            backgroundColor: "#FF7C7C",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "4%",
-            borderRadius: 7,
-            marginHorizontal: "9%",
-          }}
-        >
+        <TouchableOpacity style={styles.send_btn}>
           <Text>Send</Text>
         </TouchableOpacity>
       </View>
@@ -109,6 +98,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FBE7E3",
     justifyContent: "space-between",
+  },
+  main_innerscroll: {
+    flex: 1,
   },
   recived: {
     backgroundColor: "#fff",
@@ -147,12 +139,30 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontFamily: "Poppins-Bold",
   },
-  chatinput: {
+  chatinput_container: {
     marginHorizontal: "5%",
     flexDirection: "row",
     marginVertical: "5%",
     // alignSelf: "flex-end",
     height: "8%",
+  },
+  chatinput: {
+    backgroundColor: "#fff",
+    paddingVertical: "2%",
+    borderRadius: 7,
+    paddingHorizontal: "4%",
+    color: "black",
+    width: "85%",
+  },
+  send_btn: {
+    height: 44,
+    width: 44,
+    backgroundColor: "#FF7C7C",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "4%",
+    borderRadius: 7,
+    marginHorizontal: "9%",
   },
 });
 
